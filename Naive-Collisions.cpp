da862feae0546cd9a53aecc9f9ddd2c8cb970663
Ahmad <ahmadibadkamran@gmail.com>
2026-08-13 23:36:50 +0500

    Wrong sign in detect_resolve for left most wall collision resolution

--- Naive-Collisions.cpp
+++ Naive-Collisions.cpp
@@ -47,7 +47,7 @@ void Naive_Collisions::detect_resolve(Particle& A, float boundaries[4])
 
     if (vel.getX() < 0)
     {
-        if ((pos.getX() - radius) >= boundaries[3])
+        if ((pos.getX() - radius) <= boundaries[3])
         {
             vel.setX(vel.getX() * -1);
             A.setVelocity(vel);
