39dd6b91f20d542f805fda2de6cf1311d55b94dc
Ahmad <ahmadibadkamran@gmail.com>
2026-08-14 21:12:04 +0500

    Added Basic skeleton code for the Simulation file

---
+++ Simulation.cpp
@@ -0,0 +1,17 @@
+#include "Vector2D.h"
+#include "Particle.h"
+#include "Naive-Collisions.h"
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+       
+
+
+
+    return 0;
+}
