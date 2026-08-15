be2aa2897523b1096ff7c8e69d10d5692f92eb74
Ahmad <ahmadibadkamran@gmail.com>
2026-08-15 16:11:09 +0500

    Added a function that detects whether a button was pressed

--- Simulation.cpp
+++ Simulation.cpp
@@ -7,9 +7,35 @@
 using namespace sf;
 using namespace std;
 
+// Checks whether the user pressed a button in the simulation
+bool ButtonPressed(RectangleShape button, Vector2f click_position)
+{
+    float length = button.getSize().x;
+    float width = button.getSize().y;
+    float button_x_position = button.getPosition().x;
+    float button_y_position = button.getPosition().y;
+
+    float right_boundary = button_x_position + length / 2;
+    float left_boundary = button_x_position - length / 2;
+    
+    float upper_boundary = (button_y_position - width / 2);
+    float lower_boundary = (button_y_position + width / 2);
+
+    if (click_position.x >= left_boundary && click_position.x <= right_boundary)
+    {
+        if (click_position.y >= upper_boundary && click_position.y <= lower_boundary)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 int main()
 {
-       
+      
 
 
 
