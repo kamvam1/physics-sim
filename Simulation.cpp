0491b9838e8b74821aaaec56ab0e2670e4707564
Ahmad <ahmadibadkamran@gmail.com>
2026-08-15 21:31:39 +0500

    Centered text onto buttons

--- Simulation.cpp
+++ Simulation.cpp
@@ -51,29 +51,35 @@ void start_menu(RenderWindow& window)
     }
 
     Button buttons[3];
+    
+    buttons[0].text.setString("Start Simulation");
+    buttons[1].text.setString("Change Simulation Constants");
+    buttons[2].text.setString("Exit");
+
     for (int i = 0; i < 3; i++)
     {
-        buttons[i].rect.setSize(Vector2f(300.f, 150.f));
-        buttons[i].rect.setOrigin(150.f, 75.f);
+        buttons[i].rect.setSize(Vector2f(600.f, 150.f));
+        buttons[i].rect.setOrigin(300.f, 75.f);
 
         buttons[i].text.setFont(font);
         buttons[i].text.setCharacterSize(28);
+        FloatRect bounds = buttons[i].text.getLocalBounds();
+        buttons[i].text.setOrigin(bounds.width / 2.f + bounds.left, bounds.top + bounds.height / 2.f);
+        cout << buttons[i].text.getOrigin().x << ", " << buttons[i].text.getOrigin().y << endl; 
     }
+
     buttons[0].rect.setFillColor(Color::Green);
     buttons[0].text.setFillColor(Color::Black);
-    buttons[0].text.setString("Start Simulation");
     buttons[0].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f - 300.f));
     buttons[0].text.setPosition(buttons[0].rect.getPosition());
 
     buttons[1].rect.setFillColor(Color::Black);
     buttons[1].text.setFillColor(Color::White);
-    buttons[1].text.setString("Change Simulation Constants");
     buttons[1].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
     buttons[1].text.setPosition(buttons[1].rect.getPosition());
 
     buttons[2].rect.setFillColor(Color::Red);
     buttons[2].text.setFillColor(Color::Black);
-    buttons[2].text.setString("Exit");
     buttons[2].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f + 300.f));
     buttons[2].text.setPosition(buttons[2].rect.getPosition());
 
@@ -85,6 +91,11 @@ void start_menu(RenderWindow& window)
             if (event.type == Event::Closed)
             {
                 window.close();
+            }
+
+            if (event.type == Event::Resized)
+            {
+                
             }
 
             window.clear(Color(190, 190, 190));
@@ -103,7 +114,8 @@ void start_menu(RenderWindow& window)
 
 int main()
 {
-    RenderWindow window(VideoMode(1280, 720),"Menu");
+    RenderWindow window(VideoMode(2000, 1500),"Menu");
+    window.setFramerateLimit(30);
     start_menu(window);
 
     return 0;
