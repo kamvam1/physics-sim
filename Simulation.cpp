f251d0fa71527f47fb87ed78a3af0f257950f7fe
Ahmad <ahmadibadkamran@gmail.com>
2026-08-15 22:27:47 +0500

    Changed ButtonPressed Header, Added Basic Skeleton code for the settings menu, changed start_menu header and window.isOpen() loop.

--- Simulation.cpp
+++ Simulation.cpp
@@ -15,7 +15,8 @@ struct Button
 };
 
 // Checks whether the user pressed a button in the simulation
-bool ButtonPressed(Button b, Vector2f click_position)
+// x_pos and y_pos are both positions where the mouse click happened.
+bool ButtonPressed(Button b, float x_pos, float y_pos)
 {
     float length = b.rect.getSize().x;
     float width = b.rect.getSize().y;
@@ -28,9 +29,9 @@ bool ButtonPressed(Button b, Vector2f click_position)
     float upper_boundary = (button_y_position - width / 2);
     float lower_boundary = (button_y_position + width / 2);
 
-    if (click_position.x >= left_boundary && click_position.x <= right_boundary)
+    if (x_pos >= left_boundary && x_pos <= right_boundary)
     {
-        if (click_position.y >= upper_boundary && click_position.y <= lower_boundary)
+        if (y_pos >= upper_boundary && y_pos <= lower_boundary)
         {
             return true;
         }
@@ -39,17 +40,75 @@ bool ButtonPressed(Button b, Vector2f click_position)
     return false;
 }
 
-
-void start_menu(RenderWindow& window)
+// Allows user to change certain constants used in the simulation.
+void settings_menu(RenderWindow& window, Font& font)
 {
-    Font font;
-    if (!font.loadFromFile("../fonts/0xProtoNerdFont-Regular.ttf"))
-    {
-        cout << "Could not load font" << endl;
-        window.close();
-        return;
-    }
+    Button buttons[1];
 
+    buttons[0].text.setString("Back to Menu");
+
+
+    for (int i = 0; i < 1; i++)
+    {
+        buttons[i].rect.setSize(Vector2f(600.f, 150.f));
+        buttons[i].rect.setOrigin(300.f, 75.f);
+
+        buttons[i].text.setFont(font);
+        buttons[i].text.setCharacterSize(28);
+        FloatRect bounds = buttons[i].text.getLocalBounds();
+        buttons[i].text.setOrigin(bounds.width / 2.f + bounds.left, bounds.top + bounds.height / 2.f);
+    }
+    
+    buttons[0].rect.setFillColor(Color::Red);
+    buttons[0].text.setFillColor(Color::Black);
+    buttons[0].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
+    buttons[0].text.setPosition(buttons[0].rect.getPosition());
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == Event::Resized)
+            {
+                View view( FloatRect( Vector2f( 0.f,0.f ), Vector2f( window.getSize() ) ) );
+                window.setView(view);
+
+                buttons[0].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
+                buttons[0].text.setPosition(buttons[0].rect.getPosition());
+            }
+        }
+
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            Vector2i click_position = Mouse::getPosition(window);
+            if (ButtonPressed(buttons[0], click_position.x, click_position.y))
+            {
+                return;
+            }
+        }
+        
+        window.clear(Color(190,190,190));
+
+        for (int i = 0; i < 1; i++)
+        {
+            window.draw(buttons[i].rect);
+            window.draw(buttons[i].text);
+        }
+
+        window.display();
+    }
+}
+
+// Renders the Start menu, if start is pressed, the function returns 2, if settings is pressed it returns 1.
+// if exit is pressed window closes and function returns 0.
+int start_menu(RenderWindow& window, Font& font)
+{
     Button buttons[3];
     
     buttons[0].text.setString("Start Simulation");
@@ -65,7 +124,6 @@ void start_menu(RenderWindow& window)
         buttons[i].text.setCharacterSize(28);
         FloatRect bounds = buttons[i].text.getLocalBounds();
         buttons[i].text.setOrigin(bounds.width / 2.f + bounds.left, bounds.top + bounds.height / 2.f);
-        cout << buttons[i].text.getOrigin().x << ", " << buttons[i].text.getOrigin().y << endl; 
     }
 
     buttons[0].rect.setFillColor(Color::Green);
@@ -95,20 +153,51 @@ void start_menu(RenderWindow& window)
 
             if (event.type == Event::Resized)
             {
-                
+                View view( FloatRect( Vector2f( 0.f,0.f ), Vector2f( window.getSize() ) ) );
+                window.setView(view);
+
+                buttons[0].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f - 300.f));
+                buttons[0].text.setPosition(buttons[0].rect.getPosition());
+
+                buttons[1].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f));
+                buttons[1].text.setPosition(buttons[1].rect.getPosition());
+
+                buttons[2].rect.setPosition(Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f + 300.f));
+                buttons[2].text.setPosition(buttons[2].rect.getPosition());
+
             }
+            
 
-            window.clear(Color(190, 190, 190));
-
-            for (int i = 0; i < 3; i++)
-            {
-                window.draw(buttons[i].rect);
-                window.draw(buttons[i].text);
-            }
-
-            window.display();
         }
+        if (Mouse::isButtonPressed(Mouse::Left))
+        {
+            Vector2i click_position = Mouse::getPosition(window);
+            if (ButtonPressed(buttons[2], click_position.x, click_position.y))
+            {
+                window.close();
+            }
+            else if (ButtonPressed(buttons[1], click_position.x, click_position.y))
+            {
+                return 1;
+            }
+            else if (ButtonPressed(buttons[0], click_position.x, click_position.y))
+            {
+                return 2;
+            }
+        }
+
+        window.clear(Color(190, 190, 190));
+
+        for (int i = 0; i < 3; i++)
+        {
+            window.draw(buttons[i].rect);
+            window.draw(buttons[i].text);
+        }
+
+        window.display();
     }
+
+    return 0;
 }
 
 
@@ -116,7 +205,30 @@ int main()
 {
     RenderWindow window(VideoMode(2000, 1500),"Menu");
     window.setFramerateLimit(30);
-    start_menu(window);
+    
+    Font font;
+    if (!font.loadFromFile("../fonts/0xProtoNerdFont-Regular.ttf"))
+    {
+        cout << "Could not load font" << endl;
+        window.close();
+        return -1;
+    }
+    
+    int option = start_menu(window, font);
+    
+    while (option != 2)
+    {
+        option = start_menu(window, font);
+        
+        if (option == 0)
+        {
+            break;
+        }
+        else if (option == 1)
+        {
+            settings_menu(window, font);
+        }
+    }
 
     return 0;
 }
