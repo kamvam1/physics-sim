5a5763c44b5e341d82358400ec7cd390c47439ea
Ahmad <ahmadibadkamran@gmail.com>
2026-08-29 14:24:46 +0500

    Removed the previous particle overlap fix

--- Naive-Collisions.cpp
+++ Naive-Collisions.cpp
@@ -70,24 +70,13 @@ bool Naive_Collisions::detect(const Particle& A,const Particle& B)
     Vector2D normalAB = p1 - p2;
     normalAB = normalAB.Normalize();
 
-    Vector2D A_vel = A.getVelocity();
-    Vector2D B_vel = B.getVelocity();
-
-    float dot_A = A_vel.Dot(normalBA);
-    float dot_B = B_vel.Dot(normalAB);
-    
     float dist = p1.Dist(p2);
     if (A_radius + B_radius < dist)
     {
         return false;
     }
 
-    if (dot_A > 0 || dot_B > 0 )
-    {    
-        return true;
-    }
-
-    return false;
+    return true;
 }
 
 // Handles the post collision velocities
